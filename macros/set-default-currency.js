a894b6636a013086eb56fb3d01028cd7d04f5f34
Jakub Robaczewski <kubarob13@gmail.com>
2024-06-07 12:48:28 +0200

    Bump

---
+++ macros/set-default-currency.js
@@ -0,0 +1,142 @@
+/* ==========
+* MACRO: Coin converter
+* AUTHOR: Robak132
+* DESCRIPTION: Sets multipliers on player's currencies based on current location. Based on Old World Armoury rules [2ed].
+========== */
+
+const NATIONS = {
+  empire: "Empire",
+  bretonnia: "Bretonnia",
+  estalia: "Estalia",
+  kislev: "Kislev",
+  norsca: "Norsca",
+  tilea: "Tilea",
+  dwarf: "Dwarf Keeps",
+  elf: "Elf Kingdoms",
+  araby: "Araby"
+}
+const DATA = [
+  [1.00, 1.05, 1.10, 1.10, 1.00, 0.80, 0.90, 1.05, 1.10],
+  [0.95, 1.00, 1.05, 1.10, 1.00, 0.85, 0.85, 1.00, 1.05],
+  [0.90, 0.95, 1.00, 0.90, 1.00, 0.90, 0.80, 1.00, 1.00],
+  [0.90, 0.95, 1.10, 1.00, 0.95, 0.90, 0.90, 1.05, 1.10],
+  [0.70, 0.80, 0.90, 0.95, 1.00, 1.00, 0.80, 0.90, 1.00],
+  [0.50, 0.70, 0.80, 0.90, 1.00, 1.00, 0.00, 1.20, 0.90],
+  [1.20, 1.30, 1.40, 1.00, 0.80, 1.50, 1.00, 1.00, 0.80],
+  [0.95, 1.00, 1.05, 1.05, 0.95, 1.20, 1.00, 1.00, 0.95],
+  [0.90, 0.95, 1.00, 1.10, 1.00, 0.90, 1.20, 1.05, 1.00]
+]
+
+main();
+
+function getIdx(location) {
+  return Object.keys(NATIONS).indexOf(location);
+}
+
+async function main() {
+  await new Dialog({
+    title: "Coin converter",
+    content: `
+     <form>
+      <div class="form-group">
+        <select style="text-align: center" name="currentLocation">
+          ${Object.entries(NATIONS).map((entry) => `<option value="${entry[0]}">${entry[1]}</option>`).join("")}
+        </select>
+      </div>
+    </form>`,
+    buttons: {
+      no: {
+        icon: `<i class="fas fa-times"></i>`,
+        label: "Cancel",
+      },
+      yes: {
+        icon: `<i class="fas fa-check"></i>`,
+        label: "Next",
+        callback: async (html) => {
+          let currentLocation = html.find('[name="currentLocation"]').val();
+          await submit(currentLocation);
+        },
+      },
+    },
+    default: "yes",
+  }).render(true);
+}
+
+function extractDataFromCoin(money) {
+  let updates = {};
+  let coinValue = money.system.coinValue.value
+  let location = money.flags["wfrp4e-macros-and-more"].moneyLocation;
+  if (location === undefined) {
+    updates["flags.wfrp4e-macros-and-more.moneyLocation"] = "empire";
+    location = "empire";
+  }
+  let baseCoinValue = money.flags["wfrp4e-macros-and-more"].moneyBaseValue
+  if (baseCoinValue === undefined) {
+    updates["flags.wfrp4e-macros-and-more.moneyBaseValue"] = money.system.coinValue.value;
+    baseCoinValue = coinValue;
+  }
+  money.update(updates);
+  return {
+    location,
+    coinValue,
+    baseCoinValue,
+  };
+}
+
+async function submit(currentLocation) {
+  let coinUpdates = []
+
+  let currentLocationIdx = getIdx(currentLocation);
+  let content = `<div style="overflow-y: scroll;max-height: 500px">`
+  for (let actor of game.actors) {
+    if (!actor.itemCategories.money.length) continue
+
+    let moneyContent = ""
+    for (let money of actor.itemCategories.money) {
+      const {
+        location,
+        coinValue,
+        baseCoinValue,
+      } = extractDataFromCoin(money);
+      let convertedValue = Math.round(DATA[getIdx(location)][currentLocationIdx] * baseCoinValue)
+
+      if (money.system.quantity.value === 0 || convertedValue === coinValue) continue
+
+      moneyContent += `<div class="form-group">
+        <span style="flex: 5;text-align: center">${money.name}</span>
+        <span style="flex: 3;text-align: center">${NATIONS[location]}</span>
+        <span style="flex: 1;text-align: center">${coinValue}</span>
+        <span style="flex: 1;text-align: center">&#8594;</span>
+        <span style="flex: 1;text-align: center">${convertedValue}</span>
+      </div>`
+
+      coinUpdates.push({object: money, value: convertedValue})
+    }
+    if (moneyContent !== "") {
+      content += `<p style="text-align: center">${actor.name}</p>` + moneyContent
+    }
+  }
+  content += "</div>"
+
+  await new Dialog({
+    title: `Current Location: ${NATIONS[currentLocation]}`,
+    content: content,
+    buttons: {
+      no: {
+        icon: `<i class="fas fa-times"></i>`,
+        label: "Cancel",
+      },
+      yes: {
+        icon: `<i class="fas fa-check"></i>`,
+        label: "Proceed",
+        callback: async () => {
+          for (let entry of coinUpdates) {
+            await entry.object.update({"system.coinValue.value": entry.value})
+          }
+        },
+      },
+    },
+    default: "yes",
+  }).render(true);
+
+}
---
+++ macros/show-weapons.js
@@ -0,0 +1,57 @@
+/* ==========
+* MACRO: Show Weapons
+* VERSION: 1.0.0
+* AUTHOR: Robak132
+* DESCRIPTION: Shows actor's equipped weapons in form of status effects.
+========== */
+
+showWeapons();
+
+function showWeapons() {
+  const macro_effect = `
+  if (!game.user.isUniqueGM) return
+  effectsToCreate = [];
+  effectsToDelete = [];
+  for (let weapon of this.actor.itemCategories.weapon) {
+    let weapon_effect = this.actor.effects.find((value) => value.name === weapon.name);
+    if (weapon.equipped && weapon_effect === undefined) {
+      effectsToCreate.push({
+          name: weapon.name,
+          icon: weapon.img,
+          statuses: ['show-item'],
+          transfer: true,
+          flags: {
+            wfrp4e: {
+              preventDuplicateEffects: true,
+            },
+          },
+        });
+    } else if (!weapon.equipped && weapon_effect !== undefined) {
+      effectsToDelete.push(weapon_effect._id);
+    }
+  }
+  this.actor.createEmbeddedDocuments('ActiveEffect', effectsToCreate);
+  if (effectsToDelete.length) {
+    character.actor.deleteEmbeddedDocuments('ActiveEffect', effectsToDelete);
+  }`
+
+  if (canvas.tokens.controlled.length) {
+    for (let character of canvas.tokens.controlled) {
+      if (!character.actor.effects.find((e => e.name === game.i18n.localize('MACROS-AND-MORE.ShowWeapons')))) {
+        character.actor.createEmbeddedDocuments('ActiveEffect', [
+          {
+            name: game.i18n.localize('MACROS-AND-MORE.ShowWeapons'),
+            flags: {
+              wfrp4e: {
+                'effectTrigger': 'prePrepareItems',
+                'effectApplication': 'actor',
+                'script': macro_effect,
+              },
+            },
+          }]);
+      }
+    }
+  } else {
+    return ui.notifications.error('Select one or more characters on which you want to run this macro');
+  }
+}
