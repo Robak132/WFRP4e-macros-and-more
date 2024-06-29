8cd9294c245f9c78ffbb7892d4e6d7adfe9e3dcd
Jakub Robaczewski <kubarob13@gmail.com>
2024-06-29 16:29:59 +0200

    New building tool

--- pack.mjs
+++ pack.mjs
@@ -2,42 +2,46 @@ import path from "path";
 import fs from "fs";
 import {compilePack} from "@foundryvtt/foundryvtt-cli";
 
-const input_path = "macros";
-const json_path = "src/packs/macros";
-const output_path = "packs/macros";
-const macrosData = JSON.parse(fs.readFileSync("macros.data.json"));
+export function pack() {
+  const inputPath = "macros";
+  const jsonPath = "src/packs/macros";
+  const outputPath = "packs/macros";
+  const macrosData = JSON.parse(fs.readFileSync("macros.data.json"));
 
-function transformData(macro) {
-  const data = fs.readFileSync(path.join(input_path, macro.code_file), "utf8");
-  macro.command = data.replace(/\r\n/g, "\n");
-  macro.flags = {
-    "wfrp4e-macros-and-more": {
-      version: macro.version
-    }
-  };
-  macro = Object.keys(macro)
-    .sort()
-    .reduce((acc, c) => {
-      acc[c] = macro[c];
-      return acc;
-    }, {});
-  macro._key = `!macros!${macro._id}`;
-  delete macro.version;
-  delete macro.code_file;
-  return macro;
+  function transformData(macro) {
+    const data = fs.readFileSync(path.join(inputPath, macro.codeFile), "utf8");
+    macro.command = data.replace(/\r\n/g, "\n");
+    macro.flags = {
+      "wfrp4e-macros-and-more": {
+        version: macro.version
+      }
+    };
+    macro = Object.keys(macro)
+      .sort()
+      .reduce((acc, c) => {
+        acc[c] = macro[c];
+        return acc;
+      }, {});
+    macro._key = `!macros!${macro._id}`;
+    delete macro.version;
+    delete macro.codeFile;
+    return macro;
+  }
+
+  for (let filepath of fs.readdirSync(jsonPath)) {
+    fs.unlinkSync(path.join(jsonPath, filepath));
+  }
+  for (const macro of macrosData.macros) {
+    const fileName = `${macro.name.replace(/[^A-Za-z0-9]/gi, "_")}_${macro._id}.json`;
+    let fileData = macrosData.common;
+    fileData = Object.assign(fileData, macro);
+    fileData = transformData(fileData);
+    fs.writeFileSync(path.join(jsonPath, fileName), JSON.stringify(fileData, null, 2) + "\n", "utf8");
+  }
+
+  compilePack(jsonPath, outputPath, {log: false})
+    .then(() => console.log("Pack compilation complete."))
+    .catch((err) => console.log(err.message));
 }
 
-for (let filepath of fs.readdirSync(json_path)) {
-  fs.unlinkSync(path.join(json_path, filepath));
-}
-for (const macro of macrosData.macros) {
-  const fileName = `${macro.name.replace(/[^A-Za-z0-9]/gi, "_")}_${macro._id}.json`;
-  let fileData = macrosData.common;
-  fileData = Object.assign(fileData, macro);
-  fileData = transformData(fileData);
-  fs.writeFileSync(path.join(json_path, fileName), JSON.stringify(fileData, null, 2) + "\n", "utf8");
-}
-
-compilePack(json_path, output_path, {log: true})
-  .then(() => console.log("Pack compilation complete."))
-  .catch((err) => console.log(err.message));
+pack();
