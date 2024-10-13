a44d03784e5c1092fd24d6fbf60f435c6f3fdb41
Jakub Robaczewski <kubarob13@gmail.com>
2024-10-13 17:36:16 +0200

    Added new Consume Alcohol macro

--- scripts/macros/consume-alcohol.js
+++ scripts/macros/consume-alcohol.js
@@ -4,102 +4,181 @@
 * DESCRIPTION: Automatically counts failures and adds modifiers when drunk.
 ========== */
 
-getDrunk();
+/**
+ * @typedef {Object} Beverage
+ * @property {string} id - The unique identifier for the beverage.
+ * @property {string} difficulty - The difficulty level of consuming the beverage.
+ * @property {string} name - The name of the beverage.
+ * @property {string} strength - The strength of the beverage.
+ * @property {number} tests - The number of tests required for the beverage.
+ * @property {string} description - A description of the beverage.
+ */
 
-function findFailures() {
-  const ca1 = actor.actorEffects.filter((e) => e.conditionId === "consumealcohol1");
-  if (ca1.length) {
-    return 1;
-  }
-  const ca2 = actor.actorEffects.filter((e) => e.conditionId === "consumealcohol2");
-  if (ca2.length) {
-    return 2;
-  }
-  const ca3 = actor.actorEffects.filter((e) => e.conditionId === "consumealcohol3");
-  if (ca3.length) {
-    return ca3.length + 2;
-  }
-  return 0;
-}
-
-async function addAlcoholModifiers(actor) {
-  const failures = findFailures();
-  if (failures === 0) {
-    actor.addSystemEffect("consumealcohol1");
-  } else if (failures < 3) {
-    actor.removeSystemEffect(`consumealcohol${failures}`);
-    actor.addSystemEffect(`consumealcohol${failures + 1}`);
-  } else {
-    await actor.updateEmbeddedDocuments("ActiveEffect", [
-      {
-        _id: actor.actorEffects.find((e) => e.conditionId === "consumealcohol3" && e.disabled === false).id,
-        disabled: true
-      }
-    ]);
-    actor.addSystemEffect("consumealcohol3");
-  }
-  if (actor.characteristics.t.bonus <= failures + 1) {
-    game.tables.find((table) => table.getFlag("wfrp4e", "key") === "stinking-drunk").draw();
-  }
-}
-
-async function getDrunk() {
-  let characters = [];
-  if (game.user.character) {
-    // Get player's main character
-    characters.push(game.user.character);
-  } else if (canvas.tokens.controlled.length) {
-    // Get selected characters
-    characters = canvas.tokens.controlled.map((t) => t.actor);
-  } else {
-    return ui.notifications.error("Select one or more characters on which you want to run this macro");
+class ConsumeAlcohol extends Dialog {
+  /**
+   * Counts the number of alcohol consumption failures for the given actor.
+   * @param {Object} actor - The actor whose failures are being counted.
+   * @returns {number} The number of failures.
+   */
+  countFailures(actor) {
+    const ca1 = actor.effects.filter((e) => e.conditionId === "consumealcohol1");
+    if (ca1.length) return 1;
+    const ca2 = actor.effects.filter((e) => e.conditionId === "consumealcohol2");
+    if (ca2.length) return 2;
+    const ca3 = actor.effects.filter((e) => e.conditionId === "consumealcohol3");
+    if (ca3.length) return ca3.length + 2;
+    return 0;
   }
 
-  await new Dialog({
-    title: "Determine Beverage",
-    content: `
-     <form>
-      <div class="form-group">
-        <select style="text-align: center" name="inputStrength">
-          <option value="easy">Small Beer (+40)</option>
-          <option value="average">Ale/Dwarf Beer/Wine (+20)</option>
-          <option value="challenging">Strong Ale/Spirit/Brandy (+0)</option>
-          <option disabled>──────────</option>
-          <option value="bugman">Bugman’s XXXXXX (+20x4)</option>
-        </select>
-      </div>
-    </form>`,
-    buttons: {
-      no: {
-        icon: '<i class="fas fa-times"></i>',
-        label: "Cancel"
-      },
-      yes: {
-        icon: '<i class="fas fa-check"></i>',
-        label: "Drink",
-        callback: async (html) => {
-          let brewStrength = html.find('[name="inputStrength"]').val();
-          let tests = 1;
-          if (brewStrength === "bugman") {
-            tests = 4;
-            brewStrength = "average";
+  /**
+   * Adds or updates the alcohol consumption modifiers for the given actor.
+   * @param {Object} actor - The actor to whom the modifiers are being added.
+   */
+  async addModifiers(actor) {
+    const failures = this.countFailures(actor);
+    if (failures === 0) {
+      actor.addSystemEffect("consumealcohol1");
+    } else if (failures < 3) {
+      actor.removeSystemEffect(`consumealcohol${failures}`);
+      actor.addSystemEffect(`consumealcohol${failures + 1}`);
+    } else {
+      await actor.updateEmbeddedDocuments("ActiveEffect", [
+        {
+          _id: actor.effects.find((e) => e.conditionId === "consumealcohol3" && e.disabled === false).id,
+          disabled: true
+        }
+      ]);
+      actor.addSystemEffect("consumealcohol3");
+    }
+    if (actor.characteristics.t.bonus <= failures + 1) {
+      game.tables.find((table) => table.getFlag("wfrp4e", "key") === "stinking-drunk").draw();
+    }
+  }
+
+  /**
+   * Consumes the specified beverage for the selected characters.
+   * @param {Beverage} beverage - The beverage being consumed.
+   */
+  async consumeAlcohol(beverage) {
+    let characters = [];
+    if (game.user.character) {
+      // Get player's main character
+      characters.push(game.user.character);
+    } else if (canvas.tokens.controlled.length) {
+      // Get selected characters
+      characters = canvas.tokens.controlled.map((t) => t.actor);
+    } else {
+      return ui.notifications.error("Select one or more characters on which you want to run this macro");
+    }
+
+    for (const actor of characters) {
+      if (beverage.id === "no_roll") return await this.addModifiers(actor);
+      for (let i = 0; i < beverage.tests; i++) {
+        const test = await actor.setupSkill(game.i18n.localize("NAME.ConsumeAlcohol"), {
+          fields: {
+            difficulty: beverage.strength
           }
-
-          for (const actor of characters) {
-            for (let i = 0; i < tests; i++) {
-              const test = await actor.setupSkill(game.i18n.localize("NAME.ConsumeAlcohol"), {
-                bypass: false,
-                absolute: {difficulty: brewStrength}
-              });
-              await test.roll();
-              if (test.result.outcome === "failure") {
-                await addAlcoholModifiers(actor);
-              }
-            }
-          }
+        });
+        await test.roll();
+        if (test.result.outcome === "failure") {
+          await this.addModifiers(actor);
         }
       }
+    }
+  }
+
+  /**
+   * List of predefined beverages with their properties.
+   * @type {Beverage[]}
+   */
+  static BEVERAGES = [
+    {
+      id: "small_beer",
+      difficulty: "easy",
+      name: "Small Beer",
+      strength: "Test (+40) per pint",
+      tests: 1,
+      description: "Small beer is a weak, low-alcohol beer, often consumed by children and the poor."
     },
-    default: "yes"
-  }).render(true);
+    {
+      id: "ale",
+      difficulty: "average",
+      name: "Ale",
+      strength: "Test (+20) per pint",
+      tests: 1,
+      description: "Ale is the most common drink in the Empire, brewed in every town and village."
+    },
+    {
+      id: "dwarf_ale",
+      difficulty: "average",
+      name: "Dwarf Ale",
+      strength: "3 Tests (+20) per pint",
+      tests: 3,
+      description: "The most celebrated ales by renowned Dwarf breweries."
+    },
+    {
+      id: "bugman_ale",
+      difficulty: "average",
+      name: "Bugman’s XXXXXX Ale",
+      strength: "4 Tests (+20) per pint",
+      tests: 4,
+      description: "The most famous and potent ale in the Old World."
+    },
+    {
+      id: "wine",
+      difficulty: "average",
+      name: "Wine",
+      strength: "Test (+20) per glass",
+      tests: 1,
+      description: "Wine is a luxury, and the quality of the wine is often a sign of the wealth of the host."
+    },
+    {
+      id: "spirit",
+      difficulty: "challenging",
+      name: "Spirit",
+      strength: "Test (+0) per shot",
+      tests: 1,
+      description: "Spirits are distilled alcoholic beverages, much stronger than beer or wine."
+    },
+    {
+      id: "no_roll",
+      name: "No Roll",
+      strength: "",
+      description: "Just adds the modifiers without rolling."
+    }
+  ];
+
+  static async run() {
+    return await this.wait(
+      {
+        title: "Determine Beverage",
+        content: `<div class="directory">
+            <ol class="directory-list">
+              ${ConsumeAlcohol.BEVERAGES.map((item) => {
+                return `<a><li style="align-items: center;justify-content: center;display: flex" class="document flexrow" data-id="${item.id}">
+                    <h4 style="flex: 3"><b>${item.name}</b></h4>
+                    <h4 style="flex: 3"><i>${item.strength}</i></h4>
+                    <h4 style="flex: 5">${item.description}</h4>
+                  </li></a>`;
+              }).join("")}
+            </ol>
+          </div>`,
+        buttons: [],
+        close: () => null
+      },
+      {
+        width: 600
+      }
+    );
+  }
+
+  activateListeners(html) {
+    super.activateListeners(html);
+    html.on("click", ".document", async (ev) => {
+      let beverage = ConsumeAlcohol.BEVERAGES.find((b) => b.id === ev.currentTarget.dataset.id);
+      await this.consumeAlcohol(beverage);
+    });
+  }
 }
+
+await ConsumeAlcohol.run();
